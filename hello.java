b35211aec6c65a5baec000659b0adcec274dc374
code-raz <139544226+code-raz@users.noreply.github.com>
2023-07-14 15:00:19 +0530

    intial commit of the file

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+        public static void main(String args[]) {
+       int [] marks;
+       marks=new int[5];
+       marks[0]=100;
+       marks[1]=60;
+       marks[2]=78;
+       marks[3]=56;
+       marks[4]=89;
+       System.out.println(marks[4]);
+       
+
+    }
+}
